55fcd23be7fbe72e71ef219c43dfe31fda36742d
Caleb Zearing <czearing@outlook.com>
2021-09-27 12:06:09 -0700

    Adding storybook and temporarily introducing lage (#24)
    * Adding rating package
    
    * Adding Rating control
    
    * Adding Rating stories
    
    * Minor change to trigger diff
    
    * Reverting minor change
    
    * Introducing some addons
    
    * Resolving conflicts
    
    * Updating root package json with lage

---
+++ packages/pongo-ui/packages/react-rating/.storybook/main.js
@@ -0,0 +1,9 @@
+const root = require('../../../.storybook/main');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [...root.addons]
+}
---
+++ packages/pongo-ui/packages/react-rating/src/Rating.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Rating} from './index'
+
+export const BasicRatingExample = () => {
+    return <><Rating/></>
+}
+
+export default {
+    title: "Components/Rating",
+    component: Rating
+}
