4896ac4b53fc24dcc7af73e5841b70496b4905e1
Caleb Zearing <czearing@outlook.com>
2021-09-26 22:09:08 -0700

    Updating NX (#18)

--- packages/pongo-landing/.next/static/WfgTCRdm6wEPX0C2v1UI1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b947ad0f9ccbf8a1cc98.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
